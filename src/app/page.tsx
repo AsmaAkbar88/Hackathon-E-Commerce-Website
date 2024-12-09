e2426b73353bc6382d562fbbc0df4ebddd1dc942
Asma Akbar <masoomlarki297@gmail.com>
2024-12-09 11:25:31 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 
 
 import CompanyLogo from "./components/CompanyLogo";
-import FProducts from "./components/fProducts";
+import FProducts from "./components/FProducts";
+
 import Hero from "./components/Hero";
+import TopCat from "./components/TopCat";
 
 export default function Home() {
   return (
     <div> 
       <Hero/>
       <CompanyLogo/>
-      <FProducts/>
+      <TopCat/>
+      <FProducts/>      
     </div>
   );
 }
